c5437ff9283fc4e58dc341a857897297373712ce
VR-123-v <Vrajamohan7974@conestogac.on.ca>
2025-10-23 23:50:22 -0400

    Initial commit

---
+++ Bootcamp05NCPL16/src/main/java/com/example/Bootcamp05NCPL16/Bootcamp05Ncpl16Application.java
@@ -0,0 +1,13 @@
+package com.example.Bootcamp05NCPL16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcamp05Ncpl16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootcamp05Ncpl16Application.class, args);
+	}
+
+}
